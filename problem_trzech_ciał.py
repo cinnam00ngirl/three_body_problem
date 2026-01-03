0644ddff7d07c1c5d130292fe2b8409a75b42072
nregulsk <01197614@pw.edu.pl>
2026-01-03 16:59:21 +0100

    Dodanie funkcji definującej równanie różniczkowe

--- problem_trzech_ciał.py
+++ problem_trzech_ciał.py
@@ -9,11 +9,39 @@ m2=2
 m3=3
 
 #pozycje początkowe - tu też możemy dać suwak
-r1 = np.array([1.0, 1.0, 1.0])
-r2 = np.array([0.0, 0.0, 0.0])
-r3 = np.array([2.0, 2.0, 2.0])
+r1_0 = np.array([1.0, 1.0, 1.0])
+r2_0 = np.array([0.0, 0.0, 0.0])
+r3_0 = np.array([2.0, 2.0, 2.0])
 
-#prędkości początkowe - nie wiem jak z suwakiem, trzeba pomyśleć
-v1 = np.array([0.0, 0.5, 0.0])
-v2 = np.array([1.0, 0.0, 0.1])
-v3 = np.array([0.1, 0.1, 0.0])
+#prędkości początkowe - nie wiem jak z suwakiem, trzeba pomyśleć, bo v=dx/dt
+v1_0 = np.array([0.0, 0.5, 0.0])
+v2_0 = np.array([1.0, 0.0, 0.1])
+v3_0 = np.array([0.1, 0.1, 0.0])
+
+
+
+def rownanie_rozniczkowe(t,y,m1,m2,m3):
+    r1 = y[0:3]
+    r2 = y[3:6]
+    r3 = y[3:9]
+    v1 = y[9:12]
+    v2 = y[12:15]
+    v3 = y[15:18]
+
+    #musimy obliczyć długość wektora
+    r21_dl = np.linalg.norm(r2-r1)
+    r31_dl = np.linalg.norm(r3-r1)
+    r12_dl = r21_dl
+    r32_dl = np.linalg.norm(r3-r2)
+    r13_dl = r31_dl
+    r23_dl = r32_dl
+
+    x1_dtdt = m2*(r2-r1)/r21_dl**3 + m3*(r3-r1)/r31_dl**3
+    x2_dtdt = m1*(r1-r2)/r12_dl**3 + m3*(r3-r2)/r32_dl**3
+    x3_dtdt = m1*(r1-r3)/r13_dl**3 + m2*(r2-r3)/r23_dl**3
+
+    x1_dt = v1
+    x2_dt = v2
+    x3_dt = v3
+
+    return np.array([x1_dt, x2_dt, x3_dt, x1_dtdt, x2_dtdt, x3_dtdt])
